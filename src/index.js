f156a6e77dde65f30e4899559bf8451c740c3658
SolInvictus7x7 <dan.inosnun@gmail.com>
2026-05-14 01:27:10 -0400

    feat: Se añade vista 404 Not Found

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { router } from './router.js';
 
+
+//Archivo maneja la lógica de la SPA
 window.addEventListener('popstate', router);
 
 document.addEventListener('click', (e) => {
--- src/views/notFound.js
+++ src/views/notFound.js
@@ -1,8 +1,17 @@
 export function renderNotFound() {
   const app = document.querySelector("#app");
   app.innerHTML = `
-    <h1>404</h1>
-    <p>Ruta no encontrada.</p>
-    <p><a href="/">Volver al inicio</a></p>
+    <section class="not-found-container">
+        <h1>404</h1>
+        <p>Ruta no encontrada.</p>
+        <button id="back-btn" class="main-btn">Volver a la vista anterior</button>
+        <p><a href="/" data-route="home">O regresar al inicio</a></p>
+    </section>
   `;
+
+  // Lógica para regresar a la vista anterior
+  const backBtn = document.querySelector("#back-btn");
+  backBtn.addEventListener("click", () => {
+    window.history.back();
+  });
 }
